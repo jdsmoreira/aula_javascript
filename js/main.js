d2c709b9347cedbebd04e1b2e425a2be6f9561b7
Jeferson Duarte Souza Moreira <jeferson@ecocentauro.com.br>
2020-02-14 13:51:11 -0300

    Intrudução ao JavaScript

---
+++ js/main.js
@@ -0,0 +1,62 @@
+//Strings
+// var nome = "Jeferson D. S. Moreira";
+// var idade = 26;
+// var idade2 =  10;
+// var frase = "Japão é o melhor time do mundo";
+//alert(nome + " tem " +idade + " anos");
+//alert(idade + idade2);
+// console.log(nome);
+// console.log(idade + idade2);
+// console.log(frase.replace("Japão", "Brasil"));
+// alert(frase.replace("Japão", "Brasil"));
+// console.log(frase.toUpperCase());
+// console.log(frase.toLowerCase());
+
+//Arrays
+// var lista = ["maça", "pêra", "laranja"];
+// lista.push("uva");
+// console.log(lista);
+// console.log(lista[1]);
+// alert(lista[1]);
+// lista.pop();
+// console.log(lista);
+// console.log(lista.length);
+// console.log(lista.reverse());
+// console.log(lista.toString());
+// console.log(lista.toString()[0]);
+// console.log(lista.join(" - "));
+
+//Dicionarios
+// var fruta = {nome:"maça", cor:"vermelha"}
+// console.log(fruta);
+// console.log(fruta.nome);
+// alert(fruta.cor);
+
+// var frutas = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]
+// console.log(frutas);
+// console.log(frutas[0].nome);
+// alert(frutas[1].nome);
+
+//Condicionais , laços
+// var idade = prompt("Qual a sua idade");
+// if (idade >= 18){
+//     alert("maior de idade");
+// }else{
+//     alert("menor de idade");
+// }
+
+// var count = 0;
+// while (count <= 5){
+//     console.log(count);
+//     count++;
+// };
+
+
+// var count;
+// for(count = 1; count <= 5; count++){
+//     alert(count);
+// }
+
+// var  d = new Date();
+// alert(d.getMonth()+1);
+// alert(d.getDay());

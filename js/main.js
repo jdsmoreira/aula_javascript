e46ec700ed00621369b0f178e85ed2b799c0f4b6
Jeferson Duarte Souza Moreira <jeferson@ecocentauro.com.br>
2020-02-20 13:40:23 -0300

    finalizado curso iniciante javaScript

--- js/main.js
+++ js/main.js
@@ -60,3 +60,64 @@
 // var  d = new Date();
 // alert(d.getMonth()+1);
 // alert(d.getDay());
+
+
+//Funções
+
+// function soma(n1, n2){
+//     return n1 + n2;
+// }
+// alert(soma(5, 10));
+
+// function setReplace(frase, nome, novo_nome){
+//     frase.replace(nome, novo_nome);
+//     return frase
+// }
+
+// alert(setReplace("Vai Japão", "Japão", "Brasil"));
+
+// function validaIdade(idade){
+//     var validar;
+//     if (idade >= 18){
+//         validar =  true
+//     }else{
+//         validar = false
+//     }
+//     return validar;
+// }
+
+// var idade = prompt("Qual sua idade");
+// console.log(validaIdade(idade));
+
+
+function clicou(){
+    //alert("Obrigado por clicar");
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigado por clicar!!</b>";
+    //console.log(document.getElementById("agradecimento"));
+}
+
+function redirecionar(){
+    window.open("https://www.4devs.com.br/");
+    //mesma janela
+    //window.location.href = "https://www.4devs.com.br/";
+}
+
+function trocar(elemento){
+    
+    //document.getElementById("mousemove").innerHTML = "Obrigado por passar o mouse";
+    //alert("Trocar texto");
+    elemento.innerHTML = "Obrigado por passar o mouse";
+}
+
+function voltar(elemento){
+    //document.getElementById("mousemove").innerHTML = "Passe o mouse aqui";    
+    elemento.innerHTML = "Passe o mouse aqui";    
+}
+
+function load(){
+    alert("Página carregada");
+}
+
+function funcaoChange(elemento){
+    console.log(elemento.value);
+}
